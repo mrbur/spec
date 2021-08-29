d0fb62d3e4f8e35f66ef8d9aff8c9f905ef6b2a3
Andrei <burykinmail@gmail.com>
2021-08-29 19:11:21 +0300

    3 lab

---
+++ 3/lab3/lab3/lab3/lab3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <list>
+#include "matrix.h"
+
+using namespace std;
+
+void insertAverageToList(list<float>* floatList) {
+    float average = 0;
+    for (float num : *floatList) {
+        average += num;
+    }
+    floatList->push_back(average/floatList->size());
+}
+
+void print(list<float>* floatList) {
+    cout << endl;
+    for (float num : *floatList) {
+        cout << num << " ";
+    }
+}
+
+int main()
+{
+    //1 task
+    {
+        list<float> floatList{ 1.0, 2.0 };
+        print(&floatList);
+        insertAverageToList(&floatList);
+        print(&floatList);
+    }
+
+    //2 task
+    {
+        float** arr = new float*[2];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = new float[2];
+        }
+        int y = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = (float)(y++);
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            cout << endl;
+            for (int j = 0; j < 3; j++) {
+                cout << " " << arr[i][j];
+            }
+        }
+            
+        Matrix m(arr, 3);
+        cout << endl << endl << m.getDeterminant();
+    }
+}
