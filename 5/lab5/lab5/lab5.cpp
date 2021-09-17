bda8f894d254345bd2fb03f603928551345d9b58
Andrei <burykinmail@gmail.com>
2021-09-17 20:56:21 +0300

    2 task

--- 5/lab5/lab5/lab5.cpp
+++ 5/lab5/lab5/lab5.cpp
@@ -3,6 +3,10 @@
 #include <vector>
 #include <list>
 #include <string>
+#include <algorithm>
+#include <functional>
+
+
 using namespace std;
 
 template<typename T>
@@ -12,11 +16,16 @@ void printUnique(T begin, T end) {
     for (auto it = begin; it != end; it++) {
         stringSet.insert(*it);
     }
-    cout << endl;
     for (auto it = stringSet.begin(); it != stringSet.end(); it++) {
         cout << *it << " ";
     }
+    cout << endl;
 }
+
+struct Compare
+{
+    bool operator()(const string str, const string str1) const { return str.length() > str1.length(); }
+};
 
 int main()
 {
@@ -27,5 +36,32 @@ int main()
 
         printUnique(v.begin(), v.end());
         printUnique(l.begin(), l.end());
+    }
+
+    //2 lab
+    {
+        multiset <string, Compare> stringMultiset;
+
+        char ch;
+        string str;
+
+        string line;
+        if (getline(cin, line)) {
+            size_t pos = 0;
+            string token;
+            while ((pos = line.find(".")) != string::npos) {
+                token = line.substr(0, pos + 1);
+                if(token.length() > 1)stringMultiset.insert(token);
+                line.erase(0, pos + 1);
+            }
+        }
+        if (line.length() > 0) {
+            stringMultiset.insert(line);
+        }
+        
+        cout << endl;
+        for (auto it = stringMultiset.begin(); it != stringMultiset.end(); it++) {
+            cout << "(" << *it << ") ";
+        }
     }
 }
